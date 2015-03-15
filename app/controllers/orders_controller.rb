d8c687a18afc6ac6fb4ef3d70e85ea6a242d2d53
MadisonRong <rongweixin041@gmail.com>
2015-03-15 19:28:51 +0800

    cancel binding.pry in orderController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :signed_in_admin, only:[:statistics]
   def statistics
     Order.count_order(current_admin.id)
-    binding.pry
+    # binding.pry
     render nothing: true
   end
 end
